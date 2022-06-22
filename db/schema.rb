d59e1a262038155d9ea6ec482ecb42aa680942d4
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-22 23:18:39 +0200

    db changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_233126) do
+ActiveRecord::Schema.define(version: 2022_06_22_210923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,17 +21,29 @@ ActiveRecord::Schema.define(version: 2022_04_07_233126) do
     t.string "status"
     t.integer "rooms"
     t.integer "surface"
+    t.boolean "balcony", default: false
+    t.boolean "chimney", default: false
+    t.boolean "elevator", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "city_id", null: false
+    t.index ["city_id"], name: "index_apartments_on_city_id"
+  end
+
+  create_table "boroughs", force: :cascade do |t|
+    t.string "name"
+    t.bigint "city_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["city_id"], name: "index_boroughs_on_city_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.text "content"
-    t.bigint "apartment_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["apartment_id"], name: "index_reviews_on_apartment_id"
-  end
-
-  add_foreign_key "reviews", "apartments"
+  add_foreign_key "apartments", "cities"
+  add_foreign_key "boroughs", "cities"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,60 @@
 STATUS = ["furnished", "unfurnished"]
-ADDRESS = ["Paris, France", "Lyon, France", "London, UK", "New York, USA"]
+RARE_BOOLEAN = [true, false, false]
+BOOLEAN = [true, false]
+CITY_NAME = ["Paris", "Lyon", "Marseille", "Nice", "Bordeaux"]
 
-puts 'Destroy all flats'
-Apartment.destroy_all
-puts 'Creating Apartments...'
+LYON_BOROUGH_NAME = ["Lyon 1er", "Lyon 2e", "Lyon 3e", "Lyon 4e", "Lyon 5e", "Lyon 6e", "Lyon 7e", "Lyon 8e", "Lyon 9e"]
+PARIS_BOROUGH_NAME = ["Paris 1er", "Paris 2e", "Paris 3e", "Paris 4e", "Paris 5e", "Paris 6e", "Paris 7e", "Paris 8e",
+                      "Paris 9e", "Paris 10e", "Paris 11e", "Paris 12e", "Paris 13e", "Paris 14e", "Paris 15e",
+                      "Paris 16e", "Paris 17e", "Paris 18e", "Paris 19e", "Paris 20e"]
+MARSEILLE_BOROUGH_NAME = ["Marseille 1er", "Marseille 2e", "Marseille 3e", "Marseille 4e", "Marseille 5e",
+                          "Marseille 6e", "Marseille 7e", "Marseille 8e","Marseille 9e", "Marseille 10e",
+                          "Marseille 11e", "Marseille 12e", "Marseille 13e", "Marseille 14e", "Marseille 15e",
+                          "Marseille 16e"]
 
-50.times do
-  Apartment.create!(
-    name: Faker::Company.name,
-    address: ADDRESS.sample,
-    status: STATUS.sample,
-    rooms: rand(1..5),
-    surface: rand(15..120)
+puts 'Destroy DB'
+City.destroy_all
+
+puts 'Creating Locations...'
+
+CITY_NAME.each do |city_name|
+  City.create!(
+    name: city_name
   )
 end
 
-20.times do
-  Review.create!(
-    content: Faker::Lorem.paragraph,
-    apartment: Apartment.all.sample
+LYON_BOROUGH_NAME.each do |lyon_borough_name|
+  Borough.create!(
+    name: lyon_borough_name,
+    city: City.find_by(name: "Lyon")
+  )
+end
+PARIS_BOROUGH_NAME.each do |paris_borough_name|
+  Borough.create!(
+    name: paris_borough_name,
+    city: City.find_by(name: "Paris")
+  )
+end
+MARSEILLE_BOROUGH_NAME.each do |marseille_borough_name|
+  Borough.create!(
+    name: marseille_borough_name,
+    city: City.find_by(name: "Marseille")
+  )
+end
+
+puts 'Creating Apartments...'
+
+100.times do
+  Apartment.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    status: STATUS.sample,
+    rooms: rand(1..5),
+    surface: rand(15..120),
+    city: City.find_by(name: "Lyon"),
+    balcony: RARE_BOOLEAN.sample,
+    chimney: RARE_BOOLEAN.sample,
+    elevator: BOOLEAN.sample
   )
 end
 
