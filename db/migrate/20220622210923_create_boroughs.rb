e8817cd466d17229bfc342606d6e9f36499b599b
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-23 03:37:13 +0200

    creation des arrondisssement

--- db/migrate/20220622210923_create_boroughs.rb
+++ db/migrate/20220622210923_create_boroughs.rb
@@ -2,6 +2,7 @@ class CreateBoroughs < ActiveRecord::Migration[6.0]
   def change
     create_table :boroughs do |t|
       t.string :name
+      t.string :insee_code
       t.references :city, null: false, foreign_key: true
 
       t.timestamps
