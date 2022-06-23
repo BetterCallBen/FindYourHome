fe3db301f44b891a8a4a99f27e0d08d95a841311
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-23 03:38:06 +0200

    creation des villes

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,7 @@
 class City < ApplicationRecord
   has_many :apartments, dependent: :destroy
   has_many :boroughs, dependent: :destroy
+
+  validates :name, presence: true
+  validates :insee_code, presence: true, uniqueness: true
 end
