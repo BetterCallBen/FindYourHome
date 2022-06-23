40ac31e55b59ad5f6500dad5ee36a6839dbb242d
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-23 03:37:31 +0200

    ajout fields apartment

--- db/migrate/20220407233054_create_apartments.rb
+++ db/migrate/20220407233054_create_apartments.rb
@@ -2,13 +2,21 @@ class CreateApartments < ActiveRecord::Migration[6.0]
   def change
     create_table :apartments do |t|
       t.string :name
+      t.string :apartment_type
+      t.text :description
       t.string :address
       t.string :status
+      t.integer :price
       t.integer :rooms
       t.integer :surface
+      t.integer :borough_id
       t.boolean :balcony, default: false
       t.boolean :chimney, default: false
       t.boolean :elevator, default: false
+      t.boolean :cellar, default: false
+      t.boolean :parking, default: false
+      t.boolean :terrace, default: false
+      t.boolean :garden, default: false
       t.timestamps
     end
   end
