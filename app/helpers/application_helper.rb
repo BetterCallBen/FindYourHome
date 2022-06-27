1ff3aa528c4e6ac6aa0ebdacb97e2631cc0e3d02
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-27 02:09:24 +0200

    humanize price

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,17 @@ module ApplicationHelper
     }
     return hash[type.to_sym]
   end
+
+  def in_group_of(count, number)
+    if count == 4 || count == 5 || count == 6
+      number = number.to_s.chars
+      last_part = number.pop(3).join
+      return "#{number.join} #{last_part}"
+    elsif count == 7 || count == 8 || count == 9
+      number = number.to_s.chars
+      last_part = number.pop(3).join
+      middle_part = number.pop(3).join
+      return "#{number.join} #{middle_part} #{last_part}"
+    end
+  end
 end
