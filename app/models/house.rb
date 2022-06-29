4e6eb9be26d8ca79c012990caa31567a166bdbf7
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-29 18:50:05 +0200

    house

---
+++ app/models/house.rb
@@ -0,0 +1,10 @@
+class House < ApplicationRecord
+  belongs_to :city
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :surface, presence: true
+  validates :rooms, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true
+end
