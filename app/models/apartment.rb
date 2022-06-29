851a46cbe9a57f51c73f445a9b699c8f97d9733a
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-29 18:56:36 +0200

    improve

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -5,8 +5,6 @@ class Apartment < ApplicationRecord
   validates :address, presence: true
   validates :surface, presence: true
   validates :rooms, presence: true
-  validates :status, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
-  validates :apartment_type, presence: true, inclusion: { in: %w[flat house garage] }
   validates :description, presence: true
 end
