932508b383bbd9436477020271b64d45f94de3d2
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-07-07 15:21:31 +0200

    validates

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -2,10 +2,11 @@ class Apartment < ApplicationRecord
   belongs_to :city
   belongs_to :borough, optional: true
 
-
   validates :name, presence: true
   validates :address, presence: true
   validates :surface, presence: true
+  validates :floor, presence: true
+  validates :status, presence: true
   validates :rooms, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
