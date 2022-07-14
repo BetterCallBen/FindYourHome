137f7c41c599496c4690f633a9217ad1b16f9986
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-07-14 17:53:29 +0200

    validate bedrooms for apartments

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -7,6 +7,7 @@ class Apartment < ApplicationRecord
   validates :surface, presence: true
   validates :floor, presence: true
   validates :rooms, presence: true
+  validates :bedrooms, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
 end
