ecc9080baf5c7f2dcbc9a0446ad9bab56d1d2bf2
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-07-24 16:51:41 -0400

    remove preview in js

--- app/javascript/controllers/preview_controller.js
+++ app/javascript/controllers/preview_controller.js
@@ -3,23 +3,23 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["previewInput"]
 
-  connect() {
-    const baseUrl = document.location.href
-    this.previewInputTargets.forEach(input => {
-      if (baseUrl.includes("?")) {
-        this.url = `${baseUrl}&${input.name}=${input.value}`
-      } else {
-        this.url = `${baseUrl}?${input.name}=${input.value}`
-      }
-      fetch(this.url,
-        { method: "GET",
-          headers: { "Accept": "application/json" }
-        })
-        .then(response => response.json())
-        .then(data => {
-          document.getElementById(`preview-${input.name}`).innerText = `(${data})`
-        })
-    });
-  }
+  // connect() {
+  //   const baseUrl = document.location.href
+  //   this.previewInputTargets.forEach(input => {
+  //     if (baseUrl.includes("?")) {
+  //       this.url = `${baseUrl}&${input.name}=${input.value}`
+  //     } else {
+  //       this.url = `${baseUrl}?${input.name}=${input.value}`
+  //     }
+  //     fetch(this.url,
+  //       { method: "GET",
+  //         headers: { "Accept": "application/json" }
+  //       })
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         document.getElementById(`preview-${input.name}`).innerText = `(${data})`
+  //       })
+  //   });
+  // }
 
 }
