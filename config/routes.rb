001d4ef0684942fc948f0733bf401f61968a9ddd
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-07-24 19:12:59 -0400

    add routes for devise and research

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   get 'properties', to: 'pages#index', as: :properties
 
   resources :apartments, only: %i[show]
   resources :houses, only: %i[show]
+  resources :researches, only: %i[create]
 end
