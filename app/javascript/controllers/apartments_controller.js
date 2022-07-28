0631a02b5c256a8b716954b154e5c24c19b06616
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-07-27 22:04:04 -0400

    remove useless methods in js that are now in rails natif

--- app/javascript/controllers/apartments_controller.js
+++ app/javascript/controllers/apartments_controller.js
@@ -1,12 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "apartments", "secondRoomsInput", "secondSurfaceInput", "types", "groundFloor", "status", "sortPropositions" ]
-  static values = { apartmentTypes: Array }
-
-  connect() {
-    this.apartmentTypes = this.apartmentTypesValue
-  }
+  static targets = [ "secondSurfaceInput", "status", "sortPropositions" ]
 
   // General methods
 
@@ -20,40 +15,6 @@ export default class extends Controller {
 
   resetForm() {
     window.location = document.location.pathname
-  }
-
-  // Apartment types
-
-  toggleType(event) {
-    const type = event.currentTarget.value
-
-    if (this.apartmentTypes.includes(type)) {
-      const index = this.apartmentTypes.indexOf(type)
-      if (index > -1) {
-        this.apartmentTypes.splice(index, 1)
-      }
-    } else {
-      this.apartmentTypes.push(type)
-    }
-    this.typesTarget.value = this.apartmentTypes
-
-    this.element.submit()
-  }
-
-  // Floors
-
-  toggleGroundFloor(event) {
-    if (!event.target.checked) {
-      this.groundFloorTargets.forEach(groundFloor => {
-        groundFloor.checked = false
-      });
-    } else {
-      this.groundFloorTargets.forEach(groundFloor => {
-        groundFloor.checked = false
-      });
-      event.target.checked = true
-    }
-    this.element.submit()
   }
 
   // Status
