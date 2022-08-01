4aa7c514f5587bc4cb785f483e07ebfb56512863
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-07-31 21:18:24 -0400

    create the associated borough and cities

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -1,14 +1,18 @@
 class ResearchesController < ApplicationController
   def index
-    @researches = current_user.researches
+    @researches = current_user.researches.includes(research_cities: :city, research_boroughs: :borough)
   end
 
   def create
     @research = Research.new(research_params)
     @research.user = current_user
-    puts "research saved"
-    puts @research.inspect
-    puts "research saved"
+    Borough.where(insee_code: research_params[:locations].split(',').map(&:to_i)).each do |borough|
+      ResearchBorough.create!(research: @research, borough: borough)
+    end
+    City.where(insee_code: research_params[:locations].split(',').map(&:to_i)).each do |city|
+      ResearchCity.create!(research: @research, city: city)
+    end
+
     if @research.save
       redirect_back(fallback_location: root_path, notice: "Votre recherche a bien été enregistrée")
     else
@@ -17,6 +21,8 @@ class ResearchesController < ApplicationController
   end
 
   def research_params
-    params.require(:research).permit(:project, :types, :locations, :rooms, :bedrooms, :surface_min, :surface_max, :link, :status, :balcony, :chimney, :cellar, :garage, :terrace, :garden, :pool, :ground_floor)
+    params.require(:research).permit(:project, :types, :locations, :rooms, :bedrooms, :surface_min, :surface_max,
+                                     :link, :status, :balcony, :chimney, :cellar, :garage, :terrace, :garden, :pool,
+                                     :floor)
   end
 end
