fbab989a9e38c52645bf6be68b8c0047677dbf26
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-07-31 21:16:39 -0400

    research has many borough via a table

--- app/models/research.rb
+++ app/models/research.rb
@@ -2,4 +2,7 @@ class Research < ApplicationRecord
   belongs_to :user
   belongs_to :city, optional: true
   belongs_to :borough, optional: true
+
+  has_many :research_boroughs, dependent: :destroy
+  has_many :research_cities, dependent: :destroy
 end
