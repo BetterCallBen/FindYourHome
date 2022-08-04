88312c2a4791fd712a652a74b1b0c46fc5c4e50f
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-04 13:20:16 -0400

    cancel thing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
   end
 
   def index
-    @apartments = Apartment.includes(:city, :borough, :favorite_apartments)
+    @apartments = Apartment.includes(:city, :borough)
     @houses = House.includes(:city, :borough)
 
     filter_by_checkbox_criterias
