dff8e916862544a25c58f8c86dc3ec2d54127429
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-04 13:16:39 -0400

    add a notice controller to close them and update fav controller

--- app/javascript/controllers/notice_controller.js
+++ app/javascript/controllers/notice_controller.js
@@ -4,12 +4,7 @@ export default class extends Controller {
   static targets = []
   static values = {}
 
-  connect() {
-    console.log("toto")
+  close() {
+    this.element.remove()
   }
-
-  submitForm() {
-    this.element.submit()
-  }
-
 }
