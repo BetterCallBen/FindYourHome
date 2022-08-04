3751312a9f4c1222fcfb6a5c6e64b2605575801b
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-04 13:42:51 -0400

    fix close for alert

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -4,10 +4,6 @@ export default class extends Controller {
   static targets = []
   static values = {}
 
-  connect() {
-    console.log("toto")
-  }
-
   submitForm() {
     this.element.submit()
   }
