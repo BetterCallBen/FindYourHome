0c794836c9979812033c5fb5ab30ba307e201bc1
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-05 23:35:32 -0400

    method to add and remove from fav in js

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -1,11 +1,52 @@
 import { Controller } from "stimulus"
+import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
   static targets = []
   static values = {}
 
-  submitForm() {
-    this.element.submit()
+
+  addFavorite(event) {
+    const target = event.currentTarget
+    const apartmentId = event.currentTarget.dataset.apartmentId
+    const html = `<div data-action="click->favorite#removeFavorite" data-apartment-id="${apartmentId}">
+                    <div class="apartment-fav">
+                      <i class="fa-solid fa-heart active"></i>
+                    </div>
+                  </div>`
+    fetch(`/apartments/${apartmentId}/add_favorite`, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken(),
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      target.outerHTML = html
+    })
   }
 
+  removeFavorite(event) {
+    const apartmentId = event.currentTarget.dataset.apartmentId
+    const target = event.currentTarget
+    const html = `<div data-action="click->favorite#addFavorite" data-apartment-id="${apartmentId}">
+                <div class="apartment-fav">
+                  <i class="fa-solid fa-heart"></i>
+                </div>
+              </div>`
+    fetch(`/apartments/${apartmentId}/remove_favorite`, {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken(),
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      target.outerHTML = html
+    })
+  }
 }
