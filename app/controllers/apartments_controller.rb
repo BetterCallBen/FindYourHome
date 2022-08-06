a3dc22b5b2c3b231a0fd54263be053ece08ee923
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-06 00:26:19 -0400

    fix method to add and remove from fav

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -4,14 +4,14 @@ class ApartmentsController < ApplicationController
   end
 
   def remove_favorite
-    @apartment = Apartment.find(params[:apartment_id])
+    @apartment = Apartment.find(params[:id])
     @apartment.favorite_apartments.find_by(user: current_user).destroy
     render json: { head: :ok }
   end
 
   def add_favorite
     @favorite_apartment = FavoriteApartment.new
-    @favorite_apartment.apartment = Apartment.find(params[:apartment_id])
+    @favorite_apartment.apartment = Apartment.find(params[:id])
     @favorite_apartment.user = current_user
     @favorite_apartment.save
     render json: { head: :ok }
