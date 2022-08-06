0a7997b66770a66e958ed787407d46557a6fb57c
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-05 23:37:49 -0400

    add fav and remove fav rails method

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -2,4 +2,18 @@ class ApartmentsController < ApplicationController
   def show
     @apartment = Apartment.find(params[:id])
   end
+
+  def remove_favorite
+    @apartment = Apartment.find(params[:apartment_id])
+    @apartment.favorite_apartments.find_by(user: current_user).destroy
+    render json: { head: :ok }
+  end
+
+  def add_favorite
+    @favorite_apartment = FavoriteApartment.new
+    @favorite_apartment.apartment = Apartment.find(params[:apartment_id])
+    @favorite_apartment.user = current_user
+    @favorite_apartment.save
+    render json: { head: :ok }
+  end
 end
