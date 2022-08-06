b76001ba70def9bf0a702068912f5f7493a9dd30
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-06 00:26:02 -0400

    method to add and remove from fav

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,15 @@
 class HousesController < ApplicationController
+  def add_favorite
+    @favorite_house = FavoriteHouse.new
+    @favorite_house.house = House.find(params[:id])
+    @favorite_house.user = current_user
+    @favorite_house.save
+    render json: { head: :ok }
+  end
+
+  def remove_favorite
+    @house = House.find(params[:house_id])
+    @house.favorite_houses.find_by(user: current_user).destroy
+    render json: { head: :ok }
+  end
 end
