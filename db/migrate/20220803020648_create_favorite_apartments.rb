60154d7b791b38c65637b35c063589de6ac331a0
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-06 00:23:23 -0400

    remove name for apart fav

--- db/migrate/20220803020648_create_favorite_apartments.rb
+++ db/migrate/20220803020648_create_favorite_apartments.rb
@@ -1,9 +1,8 @@
 class CreateFavoriteApartments < ActiveRecord::Migration[6.0]
   def change
     create_table :favorite_apartments do |t|
-      t.string :name
-      t.references :apartment, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+      t.references :apartment, null: false, foreign_key: true
 
       t.timestamps
     end
