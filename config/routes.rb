5c510c901c34efbfb7f9c7e044a970650aef5f42
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-06 00:24:27 -0400

    add routes to add the house to the fav

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,18 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'properties', to: 'pages#index', as: :properties
 
-  resources :houses, only: %i[show]
+  resources :houses, only: %i[show] do
+    member do
+      post 'add_favorite', to: 'houses#add_favorite'
+      post 'remove_favorite', to: 'houses#remove_favorite'
+    end
+  end
 
   resources :apartments, only: %i[show] do
-    post 'remove_favorite', to: 'apartments#remove_favorite'
-    post 'add_favorite', to: 'apartments#add_favorite'
+    member do
+      post 'add_favorite', to: 'apartments#add_favorite'
+      post 'remove_favorite', to: 'apartments#remove_favorite'
+    end
   end
 
   resources :researches, only: %i[create index]
