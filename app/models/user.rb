bc3fbb5f55aee7d492139a52a31236ade377c594
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-15 19:49:58 +0200

    identation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :researches, dependent: :destroy
+
   has_many :favorite_apartments, dependent: :destroy
   has_many :apartments, through: :favorite_apartments
+
   has_many :favorite_houses, dependent: :destroy
   has_many :houses, through: :favorite_houses
 end
