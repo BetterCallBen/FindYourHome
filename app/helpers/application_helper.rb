814d721413367f4c3f5b05e01626bb12c5e44c92
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-17 16:11:59 +0200

    translations mathods are now in a new helper for it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,48 +42,6 @@ module ApplicationHelper
     end
   end
 
-  def translate_sort_by(sort_by)
-    hash = {
-      relevance: 'Pertinence',
-      price: 'Prix',
-      surface: 'Surface',
-      rooms: 'Pièces',
-      bedrooms: 'Chambres',
-      created_at: 'Date'
-    }
-    return hash[sort_by.to_sym]
-  end
-
-  def translate_status(status)
-    hash = {
-      furnished: 'meublé',
-      unfurnished: 'non meublé'
-    }
-    return hash[status.to_sym]
-  end
-
-  def translate_research(research)
-    hash = {
-      flat: 'Appartements',
-      house: 'Maisons',
-      rent: 'Location',
-      buy: 'Achat'
-    }
-    return hash[research.to_sym]
-  end
-
-  def translate_rooms(rooms)
-    rooms = rooms.chars.sort
-    case rooms.count
-    when 1
-      "#{rooms.first} pièce"
-    when 2
-      "#{rooms.first} ou #{rooms.last} pièces"
-    else
-      "#{rooms[0..-2].join(', ')} ou #{rooms.last} pièces"
-    end
-  end
-
   def research_locations(research)
     locations = []
     research.boroughs.each { |borough| locations << borough.name }
---
+++ app/helpers/translation_helper.rb
@@ -0,0 +1,43 @@
+module TranslationHelper
+  def translate_sort_by(sort_by)
+    hash = {
+      relevance: 'Pertinence',
+      price: 'Prix',
+      surface: 'Surface',
+      rooms: 'Pièces',
+      bedrooms: 'Chambres',
+      created_at: 'Date'
+    }
+    return hash[sort_by.to_sym]
+  end
+
+  def translate_status(status)
+    hash = {
+      furnished: 'meublé',
+      unfurnished: 'non meublé'
+    }
+    return hash[status.to_sym]
+  end
+
+  def translate_research(research)
+    hash = {
+      flat: 'Appartements',
+      house: 'Maisons',
+      rent: 'Location',
+      buy: 'Achat'
+    }
+    return hash[research.to_sym]
+  end
+
+  def translate_rooms(rooms)
+    rooms = rooms.chars.sort
+    case rooms.count
+    when 1
+      "#{rooms.first} pièce"
+    when 2
+      "#{rooms.first} ou #{rooms.last} pièces"
+    else
+      "#{rooms[0..-2].join(', ')} ou #{rooms.last} pièces"
+    end
+  end
+end
