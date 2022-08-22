a21ec5e5e4e1299d530a9dbd650718ba128e0bec
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-22 14:58:51 +0200

    message for notif favorite

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -8,12 +8,12 @@ class HousesController < ApplicationController
     @favorite_house.house = House.find(params[:id])
     @favorite_house.user = current_user
     @favorite_house.save
-    render json: { head: :ok }
+    render json: { head: :ok, message: "La maison a bien été ajouté à vos favoris" }
   end
 
   def remove_favorite
     @house = House.find(params[:id])
     @house.favorite_houses.find_by(user: current_user).destroy
-    render json: { head: :ok }
+    render json: { head: :ok, message: "La maison a bien été enlevée de vos favoris" }
   end
 end
