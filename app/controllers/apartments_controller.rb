7a541ed58a45d6982cd318a84347139ca51284ba
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-23 23:36:37 +0200

    notif when fav

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -6,7 +6,7 @@ class ApartmentsController < ApplicationController
   def remove_favorite
     @apartment = Apartment.find(params[:id])
     @apartment.favorite_apartments.find_by(user: current_user).destroy
-    render json: { head: :ok }
+    render json: { head: :ok, message: "L'appartement a bien été enlevé de vos favoris" }
   end
 
   def add_favorite
@@ -14,6 +14,6 @@ class ApartmentsController < ApplicationController
     @favorite_apartment.apartment = Apartment.find(params[:id])
     @favorite_apartment.user = current_user
     @favorite_apartment.save
-    render json: { head: :ok }
+    render json: { head: :ok, message: "L'appartement a bien été ajouté à vos favoris" }
   end
 end
