71253fb5246cd4d37271da1aceb5ab2380cc0e31
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-08-29 15:22:49 +0200

    fix when no property

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -238,9 +238,9 @@ class PagesController < ApplicationController
   end
 
   def define_what_to_display
-    if @properties.count == @properties.select { |p| p.instance_of?(Apartment) }.count
+    if @properties.count == @properties.select { |p| p.instance_of?(Apartment) }.count && @properties.count > 0
       @what = "appartement"
-    elsif @properties.count == @properties.select { |p| p.instance_of?(House) }.count
+    elsif @properties.count == @properties.select { |p| p.instance_of?(House) }.count && @properties.count > 0
       @what = "maison"
     else
       @what = "bien"
