b7dd4780d24d4a4fe058d7205ef936b7feebc95b
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-09-06 16:57:27 +0200

    markers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
   def favorites
-    @favorites = current_user.apartments + current_user.houses
+    @apartments = current_user.apartments
+    @houses = current_user.houses
+
+    @favorites = @apartments + @houses
+
+    @apartments_markers = add_markers(@apartments)
+    @houses_markers = add_markers(@houses)
+
+    @markers = @apartments_markers + @houses_markers
+  end
+
+  private
+
+  def add_markers(properties)
+    properties.geocoded.map do |property|
+      {
+        lat: property.latitude,
+        lng: property.longitude
+      }
+    end
   end
 end
